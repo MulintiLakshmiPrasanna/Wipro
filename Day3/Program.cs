3f56788a3e1adf77f54d330320eea9664142bbb7
MulintiLakshmiPrasanna <mulintilakshmiprasanna@gmail.com>
2026-01-13 13:00:42 +0530

    BankAccountManagement

---
+++ Day3/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main()
+        {
+            BankAccount acc =
+                new BankAccount("12345", "9999", 5, "HYD");
+
+            acc.Deposit(1000);
+
+            Console.WriteLine("Day 3 executed successfully");
+        }
+    }
+}
+
