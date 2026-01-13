88afe9b5c705c71f16a0ac18435918d924b7abfe
MulintiLakshmiPrasanna <mulintilakshmiprasanna@gmail.com>
2026-01-13 12:01:41 +0530

    MS Dynamics

---
+++ Day1/Day1.cs
@@ -0,0 +1,35 @@
+using System.ComponentModel.Design;
+
+namespace demo2day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("please enter the name");
+            string name = Console.ReadLine();
+            Console.WriteLine("hello" + name + "!");
+            Console.WriteLine("enter the age");
+            string ageinput = Console.ReadLine();
+            int age = int.Parse(ageinput);
+            if (age > 0)
+            {
+                if (age >= 18)
+                {
+                    Console.WriteLine("eligible");
+                }
+                else
+                {
+                    Console.WriteLine("not eligible");
+                }
+            }
+            else
+            {
+                Console.WriteLine("invalid input");
+            }
+
+
+        }
+    }
+}
